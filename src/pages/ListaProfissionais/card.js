5c982fe47ce92661417a235180a4ef206ae72f70
Douglas Shibata <douglas.shibata11@gmail.com>
2021-02-12 19:03:57 -0300

    Arrumando lista de Profissionais

--- src/pages/ListaProfissionais/card.js
+++ src/pages/ListaProfissionais/card.js
@@ -1,51 +1,46 @@
 import React from 'react'
-import { Table, TableContainer, Paper, TableHead, TableRow, TableCell, TableBody } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
+import { Card, CardActions, CardHeader, IconButton, Avatar, Grid } from '@material-ui/core';
+import { Today, PermIdentity, VideoCall } from '@material-ui/icons';
+import { Link } from 'react-router-dom';
 
-const useStyles = makeStyles({
-  table: {
-    minWidth: 350,
-  },
-});
 function DataProfissional(props) {
-  const classes = useStyles();
-  //const ehMedico = localStorage.getItem('ehMedico');
-  const items = props.items.map(item => {
-    return (
-      <TableRow key={item.id}>
-        {item.ehMedico ? <>
-          <TableCell>{item.nome}</TableCell>
-          <TableCell>{item.email}</TableCell>
-          <TableCell> {item.telefone}</TableCell>
-          <TableCell>{item.conselho} - {item.ufConselho}</TableCell>
-          <TableCell> {item.especialidade}</TableCell>
-          <TableCell>{item.registro}</TableCell>
-          <TableCell> {item.created_at}</TableCell>
-        </>
-          : <></>}
-      </TableRow>
-    )
-  })
-
   return (
-    <TableContainer component={Paper}>
-      <Table className={classes.table} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Nome</TableCell>
-            <TableCell>E-mail</TableCell>
-            <TableCell>Telefone </TableCell>
-            <TableCell>Conselho </TableCell>
-            <TableCell>Especialidade</TableCell>
-            <TableCell>Registro</TableCell>
-            <TableCell>Membro desde</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {items}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    props.data.map(data => {
+
+      return (
+        <Grid item xs={12} sm={3}>
+          <Card key={data.id}>
+            <CardHeader
+              avatar={
+                <Avatar c>
+                  {data.nome.substring(0, 1)}
+                </Avatar>
+              }
+              title={data.nome}
+              subheader={data.especialidade}
+            />
+            <CardActions disableSpacing>
+              <IconButton aria-label="Agenda">
+                <Today />
+              </IconButton>
+              <IconButton aria-label="Detalhes">
+                <Link to={`/detalhes/${data._id}`}><PermIdentity /></Link>
+
+              </IconButton>
+              <IconButton
+              style={{color:'red'}}
+                aria-label="Video"
+              >
+                <VideoCall />
+
+              </IconButton>
+            </CardActions>
+
+          </Card>
+        </Grid>
+
+      )
+    })
   )
 }
 
--- src/pages/ListaProfissionais/index.js
+++ src/pages/ListaProfissionais/index.js
@@ -3,7 +3,7 @@ import { Container, Grid, Typography } from '@material-ui/core'
 import Navbar from '../Navbar/index';
 import api from '../../services/api';
 import ChatBot from '../Chatbot';
-import MUIDataTable from "mui-datatables";
+import DataProfissional from './card';
 
 
 function FullListProfissional() {
@@ -28,129 +28,22 @@ function FullListProfissional() {
       medico.push(value)
     }
   })
-  const columns = [
-    {
-     name: "nome",
-     label: "Nome",
-     options: {
-      filter: true,
-      sort: true,
-     }
-    },
-    {
-     name: "email",
-     label: "Email",
-     options: {
-      filter: true,
-      sort: false,
-     }
-    },
-    {
-     name: "especialidade",
-     label: "Especialidade",
-     options: {
-      filter: true,
-      sort: false,
-     }
-    },
-    {
-     name: "conselho",
-     label: "Conselho",
-     options: {
-      filter: true,
-      sort: false,
-     }
-    },
-    {
-     name: "ufConselho",
-     label: "UF",
-     options: {
-      filter: true,
-      sort: false,
-     }
-    },
-    {
-     name: "registro",
-     label: "Registro",
-     options: {
-      filter: true,
-      sort: false,
-     }
-    },
-    {
-     name: "telefone",
-     label: "Telefone",
-     options: {
-      filter: true,
-      sort: false,
-     }
-    },
-   ];
-   const options={
-    download:false,
-    downloadOptions:false,
-    print:false,
-    responsive:'vertical',
-    selectableRows: 'multiple',
-    textLabels: {
-      body: {
-        noMatch: "Nenhum Profissional Encontrado",
-        toolTip: "Sort",
-        columnHeaderTooltip: column => `Sort for ${column.label}`
-      },
-      pagination: {
-        next: "Próxima Página",
-        previous: "Página Anterior",
-        rowsPerPage: "Linhas por Página:",
-        displayRows: "of",
-      },
-      toolbar: {
-        search: "Pesquisar",
-        downloadCsv: "Download CSV",
-        print: "Imprimir",
-        viewColumns: "Ver Colunas",
-        filterTable: "Filtrar Tabela",
-      },
-      filter: {
-        all: "Todos",
-        title: "Filtro",
-        reset: "LIMPAR",
-      },
-      viewColumns: {
-        title: "Mostrar Colunas",
-        titleAria: "Mostrar/Esconder Colunas da Tabela",
-      },
-      selectedRows: {
-        text: "Linhas Selecionadas",
-        delete: "Apagar",
-        deleteAria: "Apagar Linhas Selecionados",
-      },
-    }
-   }
      return (
     <>
       <Navbar />
       <Container>
         <Grid container spacing={3}>
-          <Grid item xs={12}>
+        <Grid item xs={12}>
             <h1 style={{ margin: "20px 0", color: "#589303" }}>Profissionais</h1>
             <h6 style={{ margin: "20px 0", color: "#000" }}>Encontre o profissional certo para você</h6>
           </Grid>
-          
-          <Grid item xs={12}>
           {data.length > 0 ? 
-          <MUIDataTable
-            title={"Lista de Profissionais"}
-            data={medico}
-            columns={columns}
-            options={options}
-          />
+       <DataProfissional data={medico}/>
           :
           <Typography>
             Não Há Profissionais Cadastrados
           </Typography>
           }
-        </Grid>
         </Grid>
       </Container>
       <ChatBot />
