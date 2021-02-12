e67d7c6c019826f79638310502f9a33023a51e7f
Douglas Shibata <douglas.shibata11@gmail.com>
2021-02-12 19:03:19 -0300

    Alterando Cadastro

--- src/pages/Cadastro/cadastro.js
+++ src/pages/Cadastro/cadastro.js
@@ -1,181 +1,133 @@
 import React, { useState } from 'react';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
-import { Link, useHistory } from 'react-router-dom';
-import api from "../../services/api";
-import { login } from '../../services/auth';
-import { Alert } from '@material-ui/lab';
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import PersonalInfo from './components/PersonalInfo';
+import { Link } from 'react-router-dom';
+import PersonalID from './components/PersonalID';
+import Address from './components/Address';
 import { Grid } from '@material-ui/core';
-import ReactLoading from 'react-loading';
+import ConfirmarDados from './components/Confirm';
+import HealthInfo from './components/HealthInfo';
+import Responsavel from './components/Responsavel';
 
 const useStyles = makeStyles((theme) => ({
-    paper: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-    },
-    form: {
-        width: '100%', // Fix IE 11 issue.
-        marginTop: theme.spacing(1),
-    },
-    submit: {
-        margin: theme.spacing(3, 0, 2),
-    },
-    link: {
-        width: '191px',
-        height: '21px',
-        left: '859px',
-        top: '509px',
-        fontSamily: 'Roboto',
-        fontStyle: 'normal',
-        fontWeight: 300,
-        fontSize: '18px',
-        lineHeight: '21px',
-        color: '#0071BC',
-    },
+  root: {
+    width: '100%',
+  },
+  backButton: {
+    marginRight: theme.spacing(1),
+  },
 }));
 
-export default function Cadastro() {
-    const classes = useStyles();
-    const [cpfUser, setCpfUser] = useState('');
-    const [email, setEmail] = useState('');
-    const [nome, setNome] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmPassword, setConfirmPassword] = useState('');
-    const [telefone, setTelefone] = useState('');
-    const [error, setError] = useState('')
-    const [loading, setLoading] = useState(false)
-    const history = useHistory();
-    async function handleSubmit(e) {
-        e.preventDefault();
-        const data = { cpfUser, email, password, nome, telefone }
-        try {
-            setLoading(true)
-            if (password === confirmPassword) {
-                const response = await api.post('/user', data);
-                login(response.data.token);
-                setLoading(false)
-                alert('Cadastro Realizado com sucesso')
-                history.push('/')
-            } else {
-                setLoading(false)
-                setError('Senhas Não conferem')
-            }
-        } catch (error) {
-            setLoading(false)
-            console.log(error.response.data);
-            console.log(error.response.data.message);
-            if (error.response.data.message) {
-                let mensagemErro = error.response.data.message.error;
-                setError(mensagemErro)
-                alert(mensagemErro)
-            }
-        }
+function getSteps() {
+  return ['Dados Pessoais', 'Identidade', 'Endereço', 'Plano de Saúde', 'Responsável', 'Confirmar Dados'];
+}
 
+
+
+export default function HorizontalLabelPositionBelowStepper() {
+  const classes = useStyles();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const steps = getSteps();
+
+  const [dados, setDados] = useState({
+    cpfNumber: "",
+    cpfImages: "",
+    firstName: "",
+    familyName: "",
+    socialName: "",
+    title: "",
+    email: "",
+    senha: "",
+    telefone: "",
+    rgNumber: "",
+    rgExpedition: "",
+    rgExpeditor: "",
+    rgExpeditorUf: "",
+    rgImages: "",
+    endZIP: "",
+    endState: "",
+    endCity: "",
+    endDistrict: "",
+    endDirection: "",
+    endComplement: "",
+    telefoneNumero: "",
+    telefoneTipo: "",
+    ehMedico: false,
+  })
+  const handleChange = e => {
+    setDados({
+      ...dados,
+      [e.target.name]: e.target.value
+    })
+  }
+  function getStepContent(stepIndex) {
+    switch (stepIndex) {
+      case 0:
+        return <PersonalInfo dados={dados} handleChange={handleChange} />;
+      case 1:
+        return <PersonalID dados={dados} handleChange={handleChange} />;
+      case 2:
+        return <Address dados={dados} handleChange={handleChange} />;
+      case 3:
+        return <HealthInfo dados={dados} handleChange={handleChange} />;
+      case 4:
+        return <Responsavel dados={dados} handleChange={handleChange} />;
+      case 5:
+        return <ConfirmarDados dados={dados} />;
+      default:
+        return 'Unknown stepIndex';
     }
-    return (
-        <Container component="main" maxWidth="xs">
-            <div className={classes.paper}>
-                {error &&
-                    <Alert severity="error">
-                        {error}
-                    </Alert>}
-                {loading ? <ReactLoading type={'spin'} color={'#123'} height={'20%'} width={'20%'} /> : <></>}
-                <form className={classes.form} onSubmit={handleSubmit}>
-                    <TextField
-                        type="text"
-                        variant="outlined"
-                        fullWidth
-                        label='CPF'
-                        placeholder='CPF*' minLength='11' maxLength='11' required
-                        value={cpfUser}
-                        onChange={e => setCpfUser(e.target.value)} autoFocus
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        type='email'
-                        required
-                        fullWidth
-                        id="email"
-                        label="E-mail"
-                        name="email"
-                        value={email}
-                        onChange={e => setEmail(e.target.value)}
-                        autoComplete="email"
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        type='text'
-                        required
-                        fullWidth
-                        id="nome"
-                        label="Nome"
-                        name="nome"
-                        value={nome}
-                        onChange={e => setNome(e.target.value)}
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="telefone"
-                        label="Telefone"
-                        type="text"
-                        maxLength='15' 
-                        id="telefone"
-                        onChange={e => setTelefone(e.target.value)}
-                        value={telefone}
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Senha"
-                        type="password"
-                        id="password"
-                        value={password}
-                        onChange={e => setPassword(e.target.value)}
-                        autoComplete="current-password"
-                    />
-                    <TextField
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="ConfirmPassword"
-                        label="Confirme a Senha"
-                        type="password"
-                        id="ConfirmPassword"
-                        value={confirmPassword}
-                        onChange={e => setConfirmPassword(e.target.value)}
-                        autoComplete="current-password"
-                    />
+  }
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
 
-                    <Button
-                        type="submit"
-                        fullWidth
-                        variant="contained"
-                        color="primary"
-                        className={classes.submit}
-                    >
-                        Cadastre-se
-          </Button>
-                    <Grid container>
-                        <Grid item>
-                            <Link to="/" variant="body2" className={classes.link}>
-                                Faça o seu login
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
+
+  return (
+    <div className={classes.root}>
+      <Stepper activeStep={activeStep} alternativeLabel>
+        {steps.map((label) => (
+          <Step key={label}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+      </Stepper>
+      <div>
+        {getStepContent(activeStep)}
+        <Grid container spacing={3}>
+          <Grid item sm={6}>
+            <Button
+              disabled={activeStep === 0}
+              onClick={handleBack}
+              className={classes.backButton}
+            >
+              Voltar
+              </Button>
+          </Grid>
+          <Grid item sm={6}>
+            {activeStep === steps.length - 1 ? '' :
+              <Button variant="contained" color="primary" onClick={handleNext}>
+                Continuar
+                  </Button>
+            }
+          </Grid>
+        </Grid>
+        <Link to="/" variant="body2"  >
+          Faça o seu login
                         </Link>
-                        </Grid>
-                    </Grid>
-                </form>
-            </div>
-        </Container>
-    );
+      </div>
+    </div>
+  );
 }
---
+++ src/pages/Cadastro/register.js
@@ -0,0 +1,181 @@
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { Link, useHistory } from 'react-router-dom';
+import api from "../../services/api";
+import { login } from '../../services/auth';
+import { Alert } from '@material-ui/lab';
+import { Grid } from '@material-ui/core';
+import ReactLoading from 'react-loading';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+    link: {
+        width: '191px',
+        height: '21px',
+        left: '859px',
+        top: '509px',
+        fontSamily: 'Roboto',
+        fontStyle: 'normal',
+        fontWeight: 300,
+        fontSize: '18px',
+        lineHeight: '21px',
+        color: '#0071BC',
+    },
+}));
+
+export default function Cadastro() {
+    const classes = useStyles();
+    const [cpfNumber, setcpfNumber] = useState('');
+    const [email, setEmail] = useState('');
+    const [nome, setNome] = useState('');
+    const [senha, setsenha] = useState('');
+    const [confirmsenha, setConfirmsenha] = useState('');
+    const [telefone, setTelefone] = useState('');
+    const [error, setError] = useState('')
+    const [loading, setLoading] = useState(false)
+    const history = useHistory();
+    async function handleSubmit(e) {
+        e.preventDefault();
+        const data = { cpfNumber, email, senha, nome, telefone }
+        try {
+            setLoading(true)
+            if (senha === confirmsenha) {
+                const response = await api.post('/user', data);
+                login(response.data.token);
+                setLoading(false)
+                alert('Cadastro Realizado com sucesso')
+                history.push('/')
+            } else {
+                setLoading(false)
+                setError('Senhas Não conferem')
+            }
+        } catch (error) {
+            setLoading(false)
+            console.log(error.response.data);
+            console.log(error.response.data.message);
+            if (error.response.data.message) {
+                let mensagemErro = error.response.data.message.error;
+                setError(mensagemErro)
+                alert(mensagemErro)
+            }
+        }
+
+    }
+    return (
+        <Container component="main" maxWidth="xs">
+            <div className={classes.paper}>
+                {error &&
+                    <Alert severity="error">
+                        {error}
+                    </Alert>}
+                {loading ? <ReactLoading type={'spin'} color={'#123'} height={'20%'} width={'20%'} /> : <></>}
+                <form className={classes.form} onSubmit={handleSubmit}>
+                    <TextField
+                        type="text"
+                        variant="outlined"
+                        fullWidth
+                        label='CPF'
+                        placeholder='CPF*' minLength='11' maxLength='11' required
+                        value={cpfNumber}
+                        onChange={e => setcpfNumber(e.target.value)} autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        type='email'
+                        required
+                        fullWidth
+                        id="email"
+                        label="E-mail"
+                        name="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                        autoComplete="email"
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        type='text'
+                        required
+                        fullWidth
+                        id="nome"
+                        label="Nome"
+                        name="nome"
+                        value={nome}
+                        onChange={e => setNome(e.target.value)}
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="telefone"
+                        label="Telefone"
+                        type="text"
+                        maxLength='15' 
+                        id="telefone"
+                        onChange={e => setTelefone(e.target.value)}
+                        value={telefone}
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="senha"
+                        label="Senha"
+                        type="password"
+                        id="senha"
+                        value={senha}
+                        onChange={e => setsenha(e.target.value)}
+                        autoComplete="current-senha"
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="Confirmsenha"
+                        label="Confirme a Senha"
+                        type="password"
+                        id="Confirmsenha"
+                        value={confirmsenha}
+                        onChange={e => setConfirmsenha(e.target.value)}
+                        autoComplete="current-senha"
+                    />
+
+                    <Button
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Cadastre-se
+          </Button>
+                    <Grid container>
+                        <Grid item>
+                            <Link to="/" variant="body2" className={classes.link}>
+                                Faça o seu login
+                        </Link>
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
+        </Container>
+    );
+}
